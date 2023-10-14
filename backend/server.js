4e70b87fa2b06890ae52b6d5852baa100fea4f51
lucagaetani <73741753+lucagaetani@users.noreply.github.com>
2023-10-14 15:52:23 +0200

    Added connection to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
+require('dotenv').config();
 const express = require("express");
+const mongoose = require('mongoose');
+
 const app = express();
 
 app.use(express.json());
+
+mongoose.connect(
+    process.env.MONGODB_URI,
+    { useFindAndModify: false,useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true},
+    (err) => {
+        if (err) return console.log("Error: ", err);
+        console.log("MongoDB Connection -- Ready state is:", mongoose.connection.readyState);
+    }
+);
 
 const listener = app.listen(process.env.PORT || 3000, () => {
     console.log('App is listening on port ' + listener.address().port)
